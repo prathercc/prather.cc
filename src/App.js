359f21f36d0287a5703030c1dcfa3aa7124f2866
Aaron <aprathdev@gmail.com>
2020-02-17 18:08:59 -0500

    Create DefaultDrawer component, add to App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import logo from './prathdevhd.png';
 import './App.css';
+import DefaultDrawer from './components/Default/DefaultDrawer/DefaultDrawer'
 
 function App() {
   return (
+    <>
+    <DefaultDrawer></DefaultDrawer>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
+    
+    </>
   );
 }
 
