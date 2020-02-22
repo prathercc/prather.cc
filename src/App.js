d231af54332388259eeeef91d884827362f8d258
Aaron <aprathdev@gmail.com>
2020-02-21 21:18:10 -0500

    Make DefaultLogo component responsive

--- src/App.js
+++ src/App.js
@@ -3,17 +3,18 @@ import "./App.css";
 import DefaultAppBar from "./components/Default/DefaultAppBar/DefaultAppBar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import DefaultLogo from "./components/Default/DefaultLogo/DefaultLogo";
+import { BreakpointProvider } from "react-socks";
 
 function App() {
   return (
-    <>
+    <BreakpointProvider>
       <div className="App">
         <DefaultAppBar></DefaultAppBar>
         <header className="App-header">
           <DefaultLogo></DefaultLogo>
         </header>
       </div>
-    </>
+    </BreakpointProvider>
   );
 }
 
--- src/components/Default/DefaultLogo/DefaultLogo.jsx
+++ src/components/Default/DefaultLogo/DefaultLogo.jsx
@@ -1,23 +1,64 @@
 import React from "react";
 import "./DefaultLogo.css";
-import { Image } from "react-bootstrap";
+import { Image, Container, Row, Col } from "react-bootstrap";
 import prathdevtext from "./prathdevtext.png";
 import prathdevsplat from "./prathdevsplat.png";
+import { Breakpoint } from "react-socks";
+
+const PrathImage = props => {
+  const { imageObj } = props;
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <Image
+            src={prathdevsplat}
+            style={{
+              width: imageObj.splatWidth,
+              height: "auto"
+            }}
+            rounded
+          ></Image>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Image
+            rounded
+            className={imageObj.imageClass}
+            style={{
+              width: imageObj.textWidth,
+              height: "auto"
+            }}
+            src={prathdevtext}
+          ></Image>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 function DefaultLogo() {
+  const imageObj = {
+    splatWidth: "30vw",
+    textWidth: "15vw",
+    imageClass: "App-logo-large"
+  };
   return (
     <>
-      <Image
-        src={prathdevsplat}
-        style={{ width: "30vw", height: "auto" }}
-        rounded
-      ></Image>
-      <Image
-        rounded
-        className="App-logo"
-        style={{ width: "10vw", height: "auto" }}
-        src={prathdevtext}
-      ></Image>
+      <Breakpoint large down>
+        <PrathImage
+          imageObj={{
+            ...imageObj,
+            imageClass: "App-logo-small",
+            splatWidth: "60vw",
+            textWidth: "30vw"
+          }}
+        />
+      </Breakpoint>
+      <Breakpoint xlarge up>
+        <PrathImage imageObj={{ ...imageObj }} />
+      </Breakpoint>
     </>
   );
 }
