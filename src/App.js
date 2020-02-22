903707c0790af12b8c65261e0a1b951372cfa566
Aaron <aprathdev@gmail.com>
2020-02-22 18:45:29 -0500

    Bring in styling to App.js, delete App.css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from "react";
-import "./App.css";
 import DefaultAppBar from "./components/Default/DefaultAppBar/DefaultAppBar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import DefaultLogo from "./components/Default/DefaultLogo/DefaultLogo";
 import { BreakpointProvider } from "react-socks";
 
+const appStyle = {
+  "background-color": "#666666",
+  "min-height": "100vh",
+  display: "flex",
+  "flex-direction": "column",
+  "align-items": "center",
+  "justify-content": "center",
+  "font-size": "calc(10px + 2vmin)",
+  color: "white"
+};
+
 function App() {
   return (
     <BreakpointProvider>
-      <div className="App">
+      <div style={{ "text-align": "center" }}>
         <DefaultAppBar></DefaultAppBar>
-        <header className="App-header">
+        <header style={appStyle}>
           <DefaultLogo></DefaultLogo>
         </header>
       </div>
