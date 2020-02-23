3f02bba19ad463def9ca566215613e3a678001d0
Aaron <aprathdev@gmail.com>
2020-02-22 19:27:01 -0500

    Add banner logo, make DefaultAppBar semi-responsive

--- src/components/Default/DefaultAppBar/DefaultAppBar.jsx
+++ src/components/Default/DefaultAppBar/DefaultAppBar.jsx
@@ -1,11 +1,27 @@
 import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav, Image } from "react-bootstrap";
+import prathdevbannerlogo from "./prathdevbannerlogo.png";
+import { Breakpoint } from "react-socks";
+
+const BannerImage = props => {
+  const { width } = props;
+  return (
+    <Image rounded style={{ height: "auto", width }} src={prathdevbannerlogo} />
+  );
+};
 
 function DefaultAppBar() {
   return (
     <Navbar bg="dark" variant="dark">
-      <Navbar.Brand href="#home">Prath.dev</Navbar.Brand>
-      <Nav className="mr-auto">
+      <Navbar.Brand>
+        <Breakpoint xlarge up>
+          <BannerImage width="6vw" />
+        </Breakpoint>
+        <Breakpoint large down>
+          <BannerImage width="20vw" />
+        </Breakpoint>
+      </Navbar.Brand>
+      <Nav>
         <Nav.Link href="/">Home</Nav.Link>
         <Nav.Link href="/software">Software</Nav.Link>
       </Nav>
