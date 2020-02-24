062dc3cb9c6174e5f8b4d15bad1f0a5abfd8d214
Aaron <aprathdev@gmail.com>
2020-02-23 19:48:56 -0500

    Update DefaultAppBar

--- src/components/Default/DefaultAppBar/DefaultAppBar.jsx
+++ src/components/Default/DefaultAppBar/DefaultAppBar.jsx
@@ -1,29 +1,34 @@
-import React from "react";
-import { Navbar, Nav, Image } from "react-bootstrap";
-import prathdevbannerlogo from "./prathdevbannerlogo.png";
-import { Breakpoint } from "react-socks";
+import React from 'react';
+import { Navbar, Nav, Image } from 'react-bootstrap';
+import { Breakpoint } from 'react-socks';
+import prathdevsplat from '../../../images/prathdevsplat.png';
 
 const BannerImage = props => {
   const { width } = props;
   return (
-    <Image rounded style={{ height: "auto", width }} src={prathdevbannerlogo} />
+    <Image
+      roundedCircle
+      style={{ height: 'auto', width, backgroundColor: '#666666' }}
+      src={prathdevsplat}
+    ></Image>
   );
 };
 
 function DefaultAppBar() {
   return (
-    <Navbar bg="dark" variant="dark">
+    <Navbar style={{ fontSize: 'calc(10px + 2vmin)' }} bg='dark' variant='dark'>
       <Navbar.Brand>
         <Breakpoint xlarge up>
-          <BannerImage width="6vw" />
+          <BannerImage width='4vw' />
         </Breakpoint>
         <Breakpoint large down>
-          <BannerImage width="20vw" />
+          <BannerImage width='20vw' />
         </Breakpoint>
       </Navbar.Brand>
       <Nav>
-        <Nav.Link href="/">Home</Nav.Link>
-        <Nav.Link href="/software">Software</Nav.Link>
+        <Nav.Link href='/'>Home</Nav.Link>
+        <Nav.Link disabled>/</Nav.Link>
+        <Nav.Link href='/software'>Software</Nav.Link>
       </Nav>
     </Navbar>
   );
