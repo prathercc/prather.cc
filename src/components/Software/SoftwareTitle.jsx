f669ec5a0596546c1a100af725ba85e2fc444628
Aaron <aprathdev@gmail.com>
2020-02-28 17:09:40 -0500

    Create SoftwareTitle component

---
+++ src/components/Software/SoftwareTitle.jsx
@@ -0,0 +1,49 @@
+import React, { useContext } from 'react';
+import { Image, Card } from 'react-bootstrap';
+import { Breakpoint } from 'react-socks';
+import { AppContext } from '../../AppContext';
+
+function ClickServant(props) {
+  return (
+    <>
+      <Breakpoint xlarge down>
+        <DesktopView titleObject={props.titleObject} />
+      </Breakpoint>
+    </>
+  );
+}
+
+const DesktopView = props => {
+  const appSettings = useContext(AppContext);
+  const { fgColorDetail } = appSettings;
+  console.log(appSettings);
+  const { titleObject } = props;
+  return (
+    <Card
+      style={{
+        backgroundColor: fgColorDetail,
+        fontSize: 'calc(10px + 2vmin)',
+        alignItems: 'center'
+      }}
+    >
+      <Card.Header>
+        <Image
+          rounded
+          src={titleObject.thumbnail}
+          style={{ width: titleObject.thumbnailWidth.desktop }}
+        />
+        {titleObject.title}
+      </Card.Header>
+      <Card.Img
+        style={{ width: titleObject.imageWidth.desktop }}
+        variant='top'
+        src={titleObject.image}
+      />
+      <Card.Body style={{ fontSize: 'calc(5px + 2vmin)' }}>
+        <Card.Text>{titleObject.description}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default ClickServant;
