2db43b80bae47438e8d62daf7ba9a1ca5c7f33bc
Aaron <aprathdev@gmail.com>
2020-03-01 17:52:59 -0500

    Adjust AppContext foreground/foregrounddetail colors

--- src/AppContext.js
+++ src/AppContext.js
@@ -2,8 +2,8 @@ import { createContext } from 'react';
 
 const appSettings = {
   bgColor: '#666666',
-  fgColor: '#282B5A',
-  fgColorDetail: '#313467',
+  fgColorDetail: '#282B5A',
+  fgColor: '#313467',
   textColor: '#CBCDEB',
   fontStyle: 'Tlwg Typist Bold Oblique'
 };
