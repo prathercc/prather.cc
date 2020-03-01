e9381551671747a8abcb5fa215e89e8849a28f39
Aaron <aprathdev@gmail.com>
2020-03-01 14:22:42 -0500

    Make SoftwareTitle responsive

--- src/components/Software/SoftwareTitle.jsx
+++ src/components/Software/SoftwareTitle.jsx
@@ -6,12 +6,48 @@ import { AppContext } from '../../AppContext';
 function SoftwareTitle(props) {
   return (
     <>
-      <Breakpoint xlarge down>
+      <Breakpoint large down>
+        <MobileView titleObject={props.titleObject} />
+      </Breakpoint>
+      <Breakpoint xlarge up>
         <DesktopView titleObject={props.titleObject} />
       </Breakpoint>
     </>
   );
 }
+
+const MobileView = props => {
+  const appSettings = useContext(AppContext);
+  const { fgColorDetail } = appSettings;
+  const { titleObject } = props;
+  return (
+    <Card
+      style={{
+        backgroundColor: fgColorDetail,
+        fontSize: 'calc(10px + 2vmin)',
+        alignItems: 'center'
+      }}
+    >
+      <Container>
+        <Image
+          rounded
+          src={titleObject.thumbnail}
+          style={{ width: titleObject.thumbnailWidth.mobile }}
+        />
+        {titleObject.title}
+      </Container>
+
+      <Card.Body style={{ fontSize: 'calc(5px + 2vmin)' }}>
+        <Card.Text>{titleObject.description}</Card.Text>
+        <Card.Img
+          style={{ width: titleObject.imageWidth.mobile }}
+          variant='top'
+          src={titleObject.image}
+        />
+      </Card.Body>
+    </Card>
+  );
+};
 
 const DesktopView = props => {
   const appSettings = useContext(AppContext);
