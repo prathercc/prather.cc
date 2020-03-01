cac207e374bdec4db212c6eb60acd2a1a83237aa
Aaron <aprathdev@gmail.com>
2020-03-01 17:54:03 -0500

    Updates to SoftwareDescription component

--- src/components/Software/SoftwareDescription.jsx
+++ src/components/Software/SoftwareDescription.jsx
@@ -4,8 +4,10 @@ import {
   Card,
   Accordion,
   OverlayTrigger,
-  Tooltip,
-  Popover
+  Popover,
+  Container,
+  Col,
+  Row
 } from 'react-bootstrap';
 import { Breakpoint } from 'react-socks';
 import { AppContext } from '../../AppContext';
@@ -13,12 +15,65 @@ import { AppContext } from '../../AppContext';
 function SoftwareDescription(props) {
   return (
     <>
-      <Breakpoint xlarge down>
+      <Breakpoint xlarge up>
         <DesktopView descriptionObject={props.descriptionObject} />
+      </Breakpoint>
+      <Breakpoint large down>
+        <MobileView descriptionObject={props.descriptionObject} />
       </Breakpoint>
     </>
   );
 }
+
+const MobileView = props => {
+  const appSettings = useContext(AppContext);
+  const { fgColorDetail } = appSettings;
+  const { descriptionObject } = props;
+  return (
+    <Accordion>
+      <Card
+        style={{
+          backgroundColor: fgColorDetail,
+          fontSize: 'calc(5px + 2vmin)',
+          alignItems: 'center',
+          marginTop: '5vh',
+          flexDirection: 'row'
+        }}
+      >
+        <Container>
+          <Row>
+            <Col>
+              <Accordion.Toggle as={Card.Body} eventKey='0'>
+                <Card.Header style={{ cursor: 'pointer' }}>
+                  {descriptionObject.description}
+                </Card.Header>
+              </Accordion.Toggle>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Accordion.Collapse eventKey='0'>
+                <Card.Body>
+                  <Image
+                    src={
+                      descriptionObject.image !== undefined
+                        ? descriptionObject.image
+                        : ''
+                    }
+                    style={{ width: descriptionObject.imageWidth.mobile }}
+                  />
+                  <div style={{ marginTop: '1vh' }}>
+                    {descriptionObject.content}
+                  </div>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Col>
+          </Row>
+        </Container>
+      </Card>
+    </Accordion>
+  );
+};
 
 const DesktopView = props => {
   const appSettings = useContext(AppContext);
@@ -31,35 +86,49 @@ const DesktopView = props => {
           backgroundColor: fgColorDetail,
           fontSize: 'calc(5px + 2vmin)',
           alignItems: 'center',
-          marginTop:'5vh'
+          marginTop: '5vh',
+          flexDirection: 'row'
         }}
       >
-        <Accordion.Toggle as={Card.Body} eventKey='0'>
-          <OverlayTrigger
-            placement='bottom'
-            overlay={
-                <Popover>
-                    <Popover.Title>Click to expand/collapse</Popover.Title>
-                </Popover>
-            }
-          >
-            <Card.Header>{descriptionObject.description}</Card.Header>
-          </OverlayTrigger>
-        </Accordion.Toggle>
-
-        <Accordion.Collapse eventKey='0'>
-          <Card.Body>
-            <Image
-              src={
-                descriptionObject.image !== undefined
-                  ? descriptionObject.image
-                  : ''
-              }
-              style={{ width: descriptionObject.imageWidth.desktop }}
-            />
-            <div style={{ marginTop: '1vh' }}>{descriptionObject.content}</div>
-          </Card.Body>
-        </Accordion.Collapse>
+        <Container>
+          <Row>
+            <Col>
+              <Accordion.Toggle as={Card.Body} eventKey='0'>
+                <OverlayTrigger
+                  placement='bottom'
+                  overlay={
+                    <Popover>
+                      <Popover.Title>Click to expand/collapse</Popover.Title>
+                    </Popover>
+                  }
+                >
+                  <Card.Header style={{ cursor: 'pointer' }}>
+                    {descriptionObject.description}
+                  </Card.Header>
+                </OverlayTrigger>
+              </Accordion.Toggle>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Accordion.Collapse eventKey='0'>
+                <Card.Body>
+                  <Image
+                    src={
+                      descriptionObject.image !== undefined
+                        ? descriptionObject.image
+                        : ''
+                    }
+                    style={{ width: descriptionObject.imageWidth.desktop }}
+                  />
+                  <div style={{ marginTop: '1vh' }}>
+                    {descriptionObject.content}
+                  </div>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Col>
+          </Row>
+        </Container>
       </Card>
     </Accordion>
   );
