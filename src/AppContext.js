24dbf81e46cf4df2e3ed1f74ec8f3663cf985b3d
Aaron <aprathdev@gmail.com>
2020-03-02 21:20:07 -0500

    Make DefaultAppBar more responsive, update color theme in AppContext

--- src/AppContext.js
+++ src/AppContext.js
@@ -2,8 +2,8 @@ import { createContext } from 'react';
 
 const appSettings = {
   bgColor: '#B6C0C2',
-  fgColorDetail: '#282B5A',
-  fgColor: '#313467',
+  fgColorDetail: '#2E2929',
+  fgColor: '#494949',
   textColor: '#CBCDEB',
   fontStyle: 'Tlwg Typist Bold Oblique'
 };
--- src/components/Default/DefaultAppBar/DefaultAppBar.jsx
+++ src/components/Default/DefaultAppBar/DefaultAppBar.jsx
@@ -1,18 +1,59 @@
 import React, { useContext } from 'react';
-import { Navbar, Nav, Image, NavDropdown} from 'react-bootstrap';
+import { Navbar, Nav, Image, NavDropdown } from 'react-bootstrap';
 import { Breakpoint } from 'react-socks';
-import pratherccsplat from '../../../images/pratherccsplat.png';
 import { AppContext } from '../../../AppContext';
 
 function DefaultAppBar() {
+  return (
+    <>
+      <Breakpoint large down>
+        <MobileView />
+      </Breakpoint>
+      <Breakpoint xlarge up>
+        <DesktopView />
+      </Breakpoint>
+    </>
+  );
+}
+
+const NavFiller = () => {
+  return <Nav.Link disabled> /</Nav.Link>;
+};
+
+const DesktopView = () => {
   const appSettings = useContext(AppContext);
-  const { fgColor } = appSettings;
+  const { fgColorDetail, fontStyle } = appSettings;
   return (
     <Navbar
-      style={{ fontSize: 'calc(10px + 2vmin)', backgroundColor: fgColor }}
+      style={{
+        fontSize: 'calc(10px + 2vmin)',
+        backgroundColor: fgColorDetail,
+        fontFamily: fontStyle
+      }}
       variant='dark'
     >
-      <Nav>
+      <Nav style={{ alignItems: 'center' }}>
+        <Nav.Link href='/'>Home</Nav.Link>
+        <NavFiller />
+        <Nav.Link href='/software'>Software</Nav.Link>
+      </Nav>
+    </Navbar>
+  );
+};
+
+const MobileView = () => {
+  const appSettings = useContext(AppContext);
+  const { fgColorDetail, fontStyle } = appSettings;
+  return (
+    <Navbar
+      style={{
+        fontSize: 'calc(10px + 2vmin)',
+        backgroundColor: fgColorDetail,
+        fontFamily: fontStyle
+      }}
+      variant='dark'
+    >
+      <Nav style={{ alignItems: 'center' }}>
         <NavDropdown title='Navigate'>
           <NavDropdown.Item href='/'>Home</NavDropdown.Item>
           <NavDropdown.Item href='/software'>Software</NavDropdown.Item>
@@ -20,6 +61,6 @@ function DefaultAppBar() {
       </Nav>
     </Navbar>
   );
-}
+};
 
 export default DefaultAppBar;
