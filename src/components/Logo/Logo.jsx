7cf3bc7cc190299b14fa14ef74c3d30e3581e488
Aaron <aprathdev@gmail.com>
2020-03-08 21:04:58 -0400

    Remove fast image from Logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,20 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Logo.css';
 import { Image } from 'react-bootstrap';
 import pratherccsplatBright from '../../images/pratherccsplatABS1.png';
 
 function Logo(props) {
   const { imageObj } = props;
-  const [dispClass, setDispClass] = useState('App-splat-to-mobile');
   const { alwaysFast = false } = imageObj;
   return (
     <Image
       roundedCircle
-      className={alwaysFast ? 'App-splat-to-mobile-fast' : dispClass}
+      className={alwaysFast ? 'App-splat-to-mobile-fast' : 'App-splat-to-mobile'}
       src={pratherccsplatBright}
       style={{ width: imageObj.splatWidth, filter: 'grayscale(100%)' }}
-      onMouseEnter={() => setDispClass('App-splat-to-mobile-fast')}
-      onMouseLeave={() => setDispClass(alwaysFast ? 'App-splat-to-mobile-fast' : 'App-splat-to-mobile')}
     />
   );
 }
