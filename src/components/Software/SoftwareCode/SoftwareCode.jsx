887bddc7af9a999fb1c4ee885c6d12eb98aa9a58
Aaron <aprathdev@gmail.com>
2020-03-20 21:05:05 -0400

    Add a link to github issues in SoftwareCode component

--- src/components/Software/SoftwareCode/SoftwareCode.jsx
+++ src/components/Software/SoftwareCode/SoftwareCode.jsx
@@ -1,28 +1,23 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useRef } from 'react';
 import {
-  Image,
   Card,
-  Accordion,
   Container,
   Col,
   Row,
-  Badge,
   InputGroup,
   Button,
   FormControl
 } from 'react-bootstrap';
-import { Breakpoint } from 'react-socks';
 import { AppContext } from '../../../AppContext';
-import { CloudDownload } from 'react-bootstrap-icons';
+import { CloudDownload, Alarm } from 'react-bootstrap-icons';
 import './SoftwareCode.css';
 import SoftwareModal from '../SoftwareModal/SoftwareModal';
+import { useCurrentBreakpointName } from 'react-socks';
 
 function SoftwareCode(props) {
   const appSettings = useContext(AppContext);
   const { fgColorDetail, fgColor } = appSettings;
   const { repoLink } = props;
-  const [activeColor, setActiveColor] = useState(fgColorDetail);
-  const [activeClass, setActiveClass] = useState('');
 
   return (
     <Card
@@ -31,28 +26,32 @@ function SoftwareCode(props) {
         fontSize: 'calc(10px + 2vmin)',
         alignItems: 'center',
         marginTop: '5vh',
-        flexDirection: 'row'
+        outline: '1px solid gray'
       }}
     >
       <CloneRepository repoLink={repoLink} />
+      <IssuesLink repoLink={repoLink} />
     </Card>
   );
 }
 
-const CloneRepository = props => {
+const IssuesLink = props => {
   const appSettings = useContext(AppContext);
   const { fgColorDetail, fgColor } = appSettings;
   const [activeColor, setActiveColor] = useState(fgColorDetail);
   const [activeClass, setActiveClass] = useState('');
   const { repoLink } = props;
   const [modalOpen, setModalOpen] = useState(false);
-  const [buttonText, setButtonText] = useState('Copy to Clipboard');
-  const [buttonEnabled, setButtonEnabled] = useState(true);
+  const breakpoint = useCurrentBreakpointName();
 
   const handleModalClose = () => {
     setModalOpen(false);
-    setButtonText('Copy to Clipboard');
-    setButtonEnabled(true);
+  };
+
+  const Icon = () => {
+    return (
+      <Alarm style={{ fontSize: breakpoint === 'xsmall' ? '10vw' : '5vw' }} />
+    );
   };
 
   return (
@@ -73,7 +72,84 @@ const CloneRepository = props => {
               className={activeClass}
               onClick={() => setModalOpen(true)}
             >
-              <CloudDownload style={{ color: 'yellow', fontSize: '5vw' }} />
+              <Icon />
+              Submit an Issue
+            </Card.Header>
+          </Col>
+        </Row>
+      </Container>
+      <SoftwareModal
+        title='Submit an Issue'
+        modalOpen={modalOpen}
+        handleModalClose={handleModalClose}
+        titleIcon={<Icon />}
+      >
+        <p>You are about to navigate to: </p>
+        <FormControl
+          style={{ cursor: 'text' }}
+          disabled
+          value={`${repoLink}/issues/new`}
+        />
+        <p style={{ marginTop: '1vh' }}>Are you sure you wish to continue?</p>
+        <Button
+          onClick={() => {
+            handleModalClose();
+            window.open(`${repoLink}/issues/new`);
+          }}
+          variant='dark'
+        >
+          Yes, Submit a New Issue
+        </Button>
+      </SoftwareModal>
+    </>
+  );
+};
+
+const CloneRepository = props => {
+  const appSettings = useContext(AppContext);
+  const { fgColorDetail, fgColor } = appSettings;
+  const [activeColor, setActiveColor] = useState(fgColorDetail);
+  const [activeClass, setActiveClass] = useState('');
+  const { repoLink } = props;
+  const [modalOpen, setModalOpen] = useState(false);
+  const [buttonText, setButtonText] = useState('Copy to Clipboard');
+  const [buttonEnabled, setButtonEnabled] = useState(true);
+  const textAreaRef = useRef(null);
+  const breakpoint = useCurrentBreakpointName();
+
+  const handleModalClose = () => {
+    setModalOpen(false);
+    setButtonText('Copy to Clipboard');
+    setButtonEnabled(true);
+  };
+
+  const Icon = () => {
+    return (
+      <CloudDownload
+        style={{ fontSize: breakpoint === 'xsmall' ? '10vw' : '5vw' }}
+      />
+    );
+  };
+
+  return (
+    <>
+      <Container style={{ fontSize: 'calc(15px + 2vmin)' }}>
+        <Row>
+          <Col>
+            <Card.Header
+              style={{ cursor: 'pointer', backgroundColor: activeColor }}
+              onMouseEnter={() => {
+                setActiveColor(fgColor);
+                setActiveClass('Hover-glow');
+              }}
+              onMouseLeave={() => {
+                setActiveColor(fgColorDetail);
+                setActiveClass('');
+              }}
+              className={activeClass}
+              onClick={() => setModalOpen(true)}
+            >
+              <Icon />
               Clone Repository
             </Card.Header>
           </Col>
@@ -83,15 +159,29 @@ const CloneRepository = props => {
         title='Clone Repository'
         modalOpen={modalOpen}
         handleModalClose={handleModalClose}
-        titleIcon={<CloudDownload style={{ color: 'yellow', fontSize: '5vw' }} />}
+        titleIcon={<Icon />}
       >
-        Use the link below to clone the project repository using git:
+        Use the link below to clone the project repository using{' '}
+        <a
+          href='https://git-scm.com/'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Git
+        </a>
         <InputGroup style={{ marginTop: '2vh' }}>
-          <FormControl value={`${repoLink}.git`} />
+          <FormControl
+            style={{ cursor: 'text' }}
+            disabled
+            ref={textAreaRef}
+            value={`${repoLink}.git`}
+          />
           <Button
+            visible={false}
             disabled={!buttonEnabled}
             onClick={() => {
-              navigator.clipboard.writeText(`${repoLink}.git`);
+              textAreaRef.current.select();
+              document.execCommand('copy');
               setButtonText('Copied!');
               setButtonEnabled(false);
             }}
