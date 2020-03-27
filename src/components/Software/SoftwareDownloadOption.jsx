858e15e9bd11b0d66392889565cc016ee2d165e9
Aaron <aprathdev@gmail.com>
2020-03-27 12:17:54 -0400

    Change SoftwareDownloadOption to display modal before download starts

--- src/components/Software/SoftwareDownloadOption.jsx
+++ src/components/Software/SoftwareDownloadOption.jsx
@@ -1,40 +1,65 @@
-import React, { useContext } from 'react';
-import { Button, Container, Row, Col } from 'react-bootstrap';
+import React, { useContext, useState } from 'react';
+import { Button, Container, Row, Col, Spinner } from 'react-bootstrap';
 import { useCurrentBreakpointName } from 'react-socks';
 import * as RBI from 'react-bootstrap-icons';
 import { AppContext } from '../../AppContext';
+import SoftwareModal from './SoftwareModal/SoftwareModal';
 
 function SoftwareDownloadOption(props) {
   const { downloadName, downloadLink, type } = props;
   const appSettings = useContext(AppContext);
-  const { softwareFontSize } = appSettings;
+  const { softwareFontSize, iconSizing } = appSettings;
+  const [modalOpen, setModalOpen] = useState(false);
 
   const breakpoint = useCurrentBreakpointName();
+
+  const handleModalOpen = () => {
+    setModalOpen(true);
+    window.setTimeout(() => {window.open(downloadLink); setModalOpen(false)}, 3000);
+  };
+
+  const Icon = () => {
+    return <RBI.Download style={{ fontSize: iconSizing }} />;
+  };
+
   return (
-    <Row>
-      <Col>
-        <Button
-          variant='outline-light'
-          style={{ marginTop: '1vh' }}
-          size={breakpoint === 'xsmall' ? 'sm' : 'lg'}
-          onClick={() => window.open(downloadLink)}
-          block
-        >
-          <Container>
-            <Row>
-              <Col
-                style={{
-                  fontSize: softwareFontSize
-                }}
-              >
-                {downloadName}
-              </Col>
-              <Col>{type.charAt(0).toUpperCase() + type.slice(1)}_x64</Col>
-            </Row>
-          </Container>
-        </Button>
-      </Col>
-    </Row>
+    <>
+      <Row>
+        <Col>
+          <Button
+            variant='outline-light'
+            style={{ marginTop: '1vh' }}
+            size={breakpoint === 'xsmall' ? 'sm' : 'lg'}
+            onClick={() => handleModalOpen()}
+            block
+          >
+            <Container>
+              <Row>
+                <Col
+                  style={{
+                    fontSize: softwareFontSize
+                  }}
+                >
+                  {downloadName}
+                </Col>
+                <Col>{type.charAt(0).toUpperCase() + type.slice(1)}_x64</Col>
+              </Row>
+            </Container>
+          </Button>
+        </Col>
+      </Row>
+
+      <SoftwareModal
+        title=''
+        modalOpen={modalOpen}
+        handleModalClose={()=>{}}
+        titleIcon={<Icon />}
+        closable={false}
+      >
+        <p>Starting download for {downloadName}...</p>
+        <Spinner animation='border' />
+      </SoftwareModal>
+    </>
   );
 }
 
