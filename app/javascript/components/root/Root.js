0853e2994c2cacae2b011861a04f48d4ec371835
Aaron <aprathdev@gmail.com>
2020-04-01 21:13:51 -0400

    By default disallow text highlighting

--- app/javascript/components/root/Root.js
+++ app/javascript/components/root/Root.js
@@ -3,20 +3,16 @@ import AppBar from './components/Mainpage/AppBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Display from './components/Mainpage/Display/Display';
 import Footer from './components/Mainpage/Footer';
-import { BreakpointProvider, useCurrentBreakpointName } from 'react-socks';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { AppContext } from './AppContext';
 import background from './images/background.png';
 import SoftwareRouting from './components/Routing/SoftwareRouting';
-import ClickServant from './components/Software/Applications/ClickServant/ClickServant';
-import SoftwareTable from './components/Software/SoftwareTable/SoftwareTable';
 
 function Root() {
   const appSettings = useContext(AppContext);
   const { bgColor, textColor } = appSettings;
-  const breakpoint = useCurrentBreakpointName();
-  console.log('Breakpoint: ' + breakpoint);
   return (
+    <div style={{ userSelect: 'none' }}>
       <AppContext.Provider value={appSettings}>
         <Router>
           <AppBar />
@@ -40,6 +36,7 @@ function Root() {
           <Footer />
         </Router>
       </AppContext.Provider>
+    </div>
   );
 }
 
