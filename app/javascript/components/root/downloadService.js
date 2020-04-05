a672915c453872f2f9c6524a36b921c0b59be48d
Aaron <aprathdev@gmail.com>
2020-04-05 19:43:32 -0400

    Adjust fetchDownloads, add postDownload, putDownload, deleteDownload

--- app/javascript/components/root/downloadService.js
+++ app/javascript/components/root/downloadService.js
@@ -1,12 +1,56 @@
 const host = `${window.location.origin}`;
 
 export const fetchDownloads = async (appName, setDownloads) => {
-  const response = await fetch(`${host}/api/v1/download`);
+  const response = await fetch(
+    `${host}/api/v1/download?application_name=${appName}`
+  );
   response
     .json()
     .then((resp) => {
-      const filteredData = resp.data.filter(download => download.application_name === appName);
-      setDownloads(filteredData);
+      setDownloads(resp.data);
     })
-    .catch((err) => console.log(err));
+    .catch((err) => console.err(err));
+};
+
+export const postDownload = async (download) => {
+  const response = await fetch(`${host}/api/v1/download`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(download),
+  });
+  response
+    .json()
+    .then((resp) => {
+      console.log(resp);
+    })
+    .catch((err) => console.err(err));
+};
+
+export const putDownload = async (download) => {
+  const response = await fetch(`${host}/api/v1/download/${download.id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(download),
+  });
+  response
+    .json()
+    .then((resp) => {
+    })
+    .catch((err) => console.err(err));
+
+};
+
+export const deleteDownload = async (downloadId) => {
+  const response = await fetch(`${host}/api/v1/download/${downloadId}`, {
+    method: 'DELETE',
+  });
+  response
+    .json()
+    .then((resp) => {
+    })
+    .catch((err) => console.err(err));
 };
