8439f8f1435446bcd2dac4fd8269a6393b0a9418
Aaron <aprathdev@gmail.com>
2020-04-09 21:26:21 -0400

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
   root 'main#index'
   get 'software', to: 'main#index'
   get 'software/*path', to: 'main#index'
+  get 'login', to: 'main#index'
   namespace 'api' do
     namespace 'v1' do
       resources :download
       resources :user
-      resources :sessions, only: %i[new create destroy]
+      resources :sessions, only: %i[new create destroy index]
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
