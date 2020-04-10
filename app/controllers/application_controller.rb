cad5b1e0153154b1392bddb26054e859f3b70719
Aaron <aprathdev@gmail.com>
2020-04-09 21:28:34 -0400

    Adjust protect_from_forgery line to kill pre-existing sessions without CSRF token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  protect_from_forgery prepend: true
+  protect_from_forgery with: :null_session, if: proc { |c| c.request.format == 'application/json' }
 end
