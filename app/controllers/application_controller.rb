6cd8ce9e81d7d5323c4b7767941000707eec5024
Aaron <aprathdev@gmail.com>
2020-04-10 16:05:09 -0400

    Add current_user helper function to application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@
 
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session, if: proc { |c| c.request.format == 'application/json' }
+  helper_method :current_user
+
+  def current_user
+    current_user = nil
+
+    if session[:user_id]
+      users = User.all
+      users.each do |user|
+        current_user = user if user.token == session[:user_id]
+      end
+    end
+    current_user
+  end
 end
