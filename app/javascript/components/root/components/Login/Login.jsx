b4a46816d713b29324fcac91fb3254845807966e
Aaron <aprathdev@gmail.com>
2020-04-10 16:04:10 -0400

    Add sign out button, remove defaulted login credentials

--- app/javascript/components/root/components/Login/Login.jsx
+++ app/javascript/components/root/components/Login/Login.jsx
@@ -1,25 +1,33 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { Container, Jumbotron, Form, Button } from 'react-bootstrap';
 import { AppContext } from '../../AppContext';
 import { useCurrentBreakpointName } from 'react-socks';
-import {authenticate, getSession} from '../../authService';
+import { authenticate, clearSession, getSession } from '../../authService';
 
 function Login(props) {
   const appSettings = useContext(AppContext);
   const { fgColor, fontStyle, softwareFontSize } = appSettings;
   const breakpoint = useCurrentBreakpointName();
-  const [credentials, setCredentials] = useState({email: 'test2@gmail.com', password: 'test1'})
+  const [userData, setUserData] = useState(null);
+  const [credentials, setCredentials] = useState({
+    email: '',
+    password: '',
+  });
 
   const authenticateUser = async () => {
-    console.log('Authenticating...');
-    console.log(credentials);
-    await authenticate(credentials);
-  }
+    await authenticate(credentials, setUserData);
+  };
 
-  const session = async () => {
-      console.log('current session...');
-      await getSession();
-  }
+  const signOut = async () => {
+    await clearSession(setUserData);
+  };
+
+  useEffect(() => {
+    const fetchSession = async () => {
+      await getSession(setUserData);
+    };
+    fetchSession();
+  }, []);
 
   return (
     <Container style={{ width: breakpoint === 'xlarge' ? '50vw' : '' }}>
@@ -33,33 +41,37 @@ function Login(props) {
           fontSize: softwareFontSize,
         }}
       >
-        <Form.Control
-          type='text'
-          placeholder='Email'
-          onChange={(e) => setCredentials({...credentials, email: e.target.value})}
-        />
-        <Form.Control
-          style={{ marginTop: '1vh' }}
-          type='password'
-          placeholder='Password'
-          onChange={(e) => setCredentials({...credentials, password: e.target.value})}
-        />
+        {userData ? (
+          <Button onClick={() => signOut()} variant='warning'>
+            Sign Out
+          </Button>
+        ) : (
+          <>
+            <Form.Control
+              type='text'
+              placeholder='Email'
+              onChange={(e) =>
+                setCredentials({ ...credentials, email: e.target.value })
+              }
+            />
+            <Form.Control
+              style={{ marginTop: '1vh' }}
+              type='password'
+              placeholder='Password'
+              onChange={(e) =>
+                setCredentials({ ...credentials, password: e.target.value })
+              }
+            />
 
-        <Button
-          onClick={() => authenticateUser()}
-          style={{ marginTop: '1vh' }}
-          variant='warning'
-        >
-          Sign In
-        </Button>
-        <p />
-        <Button
-          onClick={() => session()}
-          style={{ marginTop: '1vh' }}
-          variant='warning'
-        >
-          Current Session
-        </Button>
+            <Button
+              onClick={() => authenticateUser()}
+              style={{ marginTop: '1vh' }}
+              variant='warning'
+            >
+              Sign In
+            </Button>
+          </>
+        )}
       </Jumbotron>
     </Container>
   );
