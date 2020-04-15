13849ed1999bb5b22a16706c0294706d778015fb
Aaron <aprathdev@gmail.com>
2020-04-15 19:10:57 -0400

    Merge SoftwareTableRow and SoftwareSample into SoftwareTable

--- app/javascript/components/root/components/Software/SoftwareTable/SoftwareTable.jsx
+++ app/javascript/components/root/components/Software/SoftwareTable/SoftwareTable.jsx
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { Table, Button, Spinner } from 'react-bootstrap';
+import { Table, Button, Spinner, Badge, Image } from 'react-bootstrap';
 import SoftwarePage from '../SoftwarePage/SoftwarePage';
 import { fetchAllSoftware } from '../../../softwareService';
-import SoftwareSample from '../SoftwareApplication/SoftwareSample';
-
+import { useCurrentBreakpointName } from 'react-socks';
 function SoftwareTable(props) {
   const { userData } = props;
   const [software, setSoftware] = useState(null);
@@ -33,12 +32,18 @@ function SoftwareTable(props) {
                 <th>Name</th>
                 <th>Language</th>
                 <th>Platform(s)</th>
-                {userData ? <th></th> : ''}
+                <th></th>
               </tr>
             </thead>
             <tbody>
               {software.map((app) => {
-                return <SoftwareSample key={app.id} value={app} userData={userData}/>;
+                return (
+                  <SoftwareSample
+                    key={app.id}
+                    value={app}
+                    userData={userData}
+                  />
+                );
               })}
             </tbody>
           </Table>
@@ -60,4 +65,63 @@ function SoftwareTable(props) {
     </SoftwarePage>
   );
 }
+
+const SoftwareSample = (props) => {
+  const { value, userData } = props;
+  const breakpoint = useCurrentBreakpointName();
+  const compatibility = {
+    windows: value.windows,
+    linux: value.linux,
+    mac: value.mac,
+    android: value.android,
+    ios: value.ios,
+  };
+
+  return (
+    <tr style={{ cursor: 'pointer' }}>
+      <td onClick={() => window.open(`/software/${value.name}`, '_self')}>
+        {' '}
+        <Image
+          rounded
+          src={value.icon_link}
+          style={{
+            width:
+              breakpoint === 'xsmall'
+                ? '5vw'
+                : breakpoint === 'medium'
+                ? '4vw'
+                : '2vw',
+          }}
+        />{' '}
+        {value.name}
+      </td>
+      <td onClick={() => window.open(`/software/${value.name}`, '_self')}>
+        {value.languages}
+      </td>
+      <td onClick={() => window.open(`/software/${value.name}`, '_self')}>
+        {compatibility.windows ? <Badge variant='light'>Windows</Badge> : ''}{' '}
+        {compatibility.linux ? <Badge variant='warning'>Linux</Badge> : ''}{' '}
+        {compatibility.mac ? <Badge variant='danger'>Mac</Badge> : ''}{' '}
+        {compatibility.android ? <Badge variant='info'>Android</Badge> : ''}{' '}
+        {compatibility.ios ? <Badge variant='success'>iOS</Badge> : ''}
+      </td>
+      {userData ? (
+        <td>
+          <Button
+            onClick={() =>
+              window.open(`/software/admin/edit/${value.name}`, '_self')
+            }
+            size='sm'
+            block
+            variant='warning'
+          >
+            Edit
+          </Button>
+        </td>
+      ) : (
+        <td></td>
+      )}
+    </tr>
+  );
+};
 export default SoftwareTable;
