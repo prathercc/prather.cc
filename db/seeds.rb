1cf77f4775ee27ee7c3f9dddaa3c93d3584c9f3a
Aaron <aprathdev@gmail.com>
2020-04-18 18:04:47 -0400

    Remove seeded download

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,14 +8,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 1.times do
-  Download.create({
-                    application_name: 'Click-Servant',
-                    path: 'http://prather.cc/somelink.ewxe',
-                    os_type: 'Windows',
-                    file_size: '5.3mb',
-                    file_name: 'ClickServant.exe',
-                    download_count: 0
-                  })
   User.create({
                 email: 'admin@prather.cc',
                 password: 'test',
