2995f104c70aa45ab2928febecfe7288707b6eaa
Aaron <aprathdev@gmail.com>
2020-04-18 18:12:31 -0400

    Pass down entire app obj in props

--- app/javascript/components/root/components/Software/SoftwareCompatibility/SoftwareCompatibility.jsx
+++ app/javascript/components/root/components/Software/SoftwareCompatibility/SoftwareCompatibility.jsx
@@ -1,14 +1,14 @@
 import React, { useContext, useState, useEffect } from 'react';
-import {Card,Table,Button,Container,Spinner,Form,Row,Col,} from 'react-bootstrap';
+import { Card, Table, Button, Container, Spinner, Form, Row, Col, } from 'react-bootstrap';
 import { AppContext } from '../../../AppContext';
 import { Check, X } from 'react-bootstrap-icons';
 import SoftwareModal from '../SoftwareModal/SoftwareModal';
-import {fetchDownloads,postDownload,putDownload,deleteDownload} from '../../../downloadService';
+import { fetchDownloads, postDownload, putDownload, deleteDownload } from '../../../downloadService';
 
 function SoftwareCompatibility(props) {
   const appSettings = useContext(AppContext);
   const { fgColorDetail } = appSettings;
-  const { compatibility, appName, setMainDownloads, userData } = props;
+  const { compatibility, app, setMainDownloads, userData } = props;
 
   return (
     <Card
@@ -24,12 +24,12 @@ function SoftwareCompatibility(props) {
         <CompatibilityTable compatibility={compatibility} />
         {userData ? (
           <EditDownloads
-            appName={appName}
+            app={app}
             setMainDownloads={setMainDownloads}
           />
         ) : (
-          ''
-        )}
+            ''
+          )}
         <Container>
           <strong>Download(s)</strong>
           {props.children ? props.children : <p>N/A</p>}
@@ -40,7 +40,7 @@ function SoftwareCompatibility(props) {
 }
 
 const EditDownloads = (props) => {
-  const { appName, setMainDownloads } = props;
+  const { app, setMainDownloads } = props;
   const [modalOpen, setModalOpen] = useState(false);
   const [downloads, setDownloads] = useState(null);
   const [downloadsLoading, setDownloadsLoading] = useState(true);
@@ -48,12 +48,12 @@ const EditDownloads = (props) => {
   const handleModalClose = async () => {
     setModalOpen(false);
     setDownloadsLoading(true);
-    await fetchDownloads(appName, setMainDownloads);
+    await fetchDownloads(app.id, setMainDownloads);
   };
 
   const openAndLoadDownloads = async () => {
     setModalOpen(true);
-    await fetchDownloads(appName, setDownloads);
+    await fetchDownloads(app.id, setDownloads);
   };
 
   useEffect(() => {
@@ -65,7 +65,7 @@ const EditDownloads = (props) => {
   return (
     <>
       <Button onClick={() => openAndLoadDownloads()} variant='warning' block>
-        Modify {appName} Downloads
+        Modify {app.name} Downloads
       </Button>
       <SoftwareModal
         title='Modify Downloads'
@@ -75,33 +75,33 @@ const EditDownloads = (props) => {
         {downloadsLoading ? (
           <Spinner animation='border' />
         ) : (
-          downloads.map((download) => {
-            return (
-              <Download
-                key={download.id}
-                value={download}
-                appName={appName}
-                reloadDownloads={openAndLoadDownloads}
-              />
-            );
-          })
-        )}
-        <Download appName={appName} reloadDownloads={openAndLoadDownloads} />
+            downloads.map((download) => {
+              return (
+                <Download
+                  key={download.id}
+                  value={download}
+                  app={app}
+                  reloadDownloads={openAndLoadDownloads}
+                />
+              );
+            })
+          )}
+        <Download app={app} reloadDownloads={openAndLoadDownloads} />
       </SoftwareModal>
     </>
   );
 };
 
 const Download = (props) => {
-  const { value, appName, reloadDownloads } = props;
-
+  const { value, app, reloadDownloads } = props;
   const blankDownload = {
-    application_name: appName,
+    application_name: app.name,
     file_name: '',
     file_size: '',
     os_type: '',
     path: '',
-    download_count: 0
+    download_count: 0,
+    software_id: app.id
   };
 
   const [download, setDownload] = useState(
@@ -185,17 +185,17 @@ const Download = (props) => {
                 Add
               </Button>
             ) : (
-              <Button onClick={() => handleEditDownload()} variant='success'>
-                Save
-              </Button>
-            )}
+                <Button onClick={() => handleEditDownload()} variant='success'>
+                  Save
+                </Button>
+              )}
             {value === undefined ? (
               ''
             ) : (
-              <Button onClick={() => handleDeleteDownload()} variant='danger'>
-                Delete
-              </Button>
-            )}
+                <Button onClick={() => handleDeleteDownload()} variant='danger'>
+                  Delete
+                </Button>
+              )}
           </Row>
         </Container>
       </Form>
@@ -259,14 +259,14 @@ const CompatiblityResult = (props) => {
           }}
         />
       ) : (
-        <X
-          style={{
-            color: 'red',
-            fontSize: iconSizing,
-            filter: 'grayscale(0.5)',
-          }}
-        />
-      )}
+          <X
+            style={{
+              color: 'red',
+              fontSize: iconSizing,
+              filter: 'grayscale(0.5)',
+            }}
+          />
+        )}
     </td>
   );
 };
