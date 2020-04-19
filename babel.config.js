3558b5a40f647f51d1b72ba227f738424668d500
Aaron <aprathdev@gmail.com>
2020-04-18 22:43:02 -0400

    Disable babel-plugin-transform-react-remove-prop-types, causes error in prod

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
-module.exports = function(api) {
+module.exports = function (api) {
   var validEnv = ['development', 'test', 'production']
   var currentEnv = api.env()
   var isDevelopmentEnv = api.env('development')
@@ -8,10 +8,10 @@ module.exports = function(api) {
   if (!validEnv.includes(currentEnv)) {
     throw new Error(
       'Please specify a valid `NODE_ENV` or ' +
-        '`BABEL_ENV` environment variables. Valid values are "development", ' +
-        '"test", and "production". Instead, received: ' +
-        JSON.stringify(currentEnv) +
-        '.'
+      '`BABEL_ENV` environment variables. Valid values are "development", ' +
+      '"test", and "production". Instead, received: ' +
+      JSON.stringify(currentEnv) +
+      '.'
     )
   }
 
@@ -75,13 +75,14 @@ module.exports = function(api) {
         {
           async: false
         }
-      ],
-      isProductionEnv && [
-        'babel-plugin-transform-react-remove-prop-types',
-        {
-          removeImport: true
-        }
       ]
+      //Disabled as per - https://stackoverflow.com/questions/60267624/bootstrap-modal-causes-uncaught-typeerror-cannot-convert-undefined-or-null-to
+      // isProductionEnv && [
+      //   'babel-plugin-transform-react-remove-prop-types',
+      //   {
+      //     removeImport: true
+      //   }
+      // ]
     ].filter(Boolean)
   }
 }
