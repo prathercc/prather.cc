82b6671916b0e5e445e6339682bdaa4b9ee16148
aaron <aprathdev@gmail.com>
2020-05-11 20:44:20 -0400

    Create the FloatText subcomponent in Display.

--- app/javascript/components/root/components/Mainpage/Display/Display.jsx
+++ app/javascript/components/root/components/Mainpage/Display/Display.jsx
@@ -5,6 +5,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { Breakpoint } from 'react-socks';
 import Logo from '../../Logo/Logo';
+import { StandardCard } from '../../Utility/Utility';
 
 function Display() {
   return (
@@ -19,17 +20,31 @@ function Display() {
         <PrathImage splatWidth='45vw' />
       </Breakpoint>
       <Breakpoint xlarge up>
-        <PrathImage splatWidth='30vw' />
+        <PrathImage splatWidth='25vw' />
       </Breakpoint>
+      <FloatText />
     </>
   );
+}
+
+const FloatText = () => {
+  return (
+    <StandardCard style={{ opacity: 0.8, width: '100%', margin: 'auto' }}>
+      <div style={{ fontSize: 'calc(6.5px + 2vmin)', color: 'white' }}>
+        Welcome to
+        {' <'}
+        <div style={{ color: '#4fc9c9', display: 'inline' }}>Prather.cc</div>
+        {' />'}
+      </div>
+    </StandardCard>
+  )
 }
 
 const PrathImage = props => {
   const { splatWidth } = props;
 
   return (
-    <Container className='App-splat-to-mobile' style={{ opacity: .9 }}>
+    <Container className='App-splat-to-mobile' style={{ opacity: .85 }}>
       <Row>
         <Col>
           <Logo splatWidth={splatWidth} />
