b2ae9206487429f31199c3bf4c218b052da67b63
aaron <aprathdev@gmail.com>
2020-07-01 23:41:36 -0400

    Refactor SoftwareTable

--- app/javascript/components/root/components/Software/SoftwareTable/SoftwareTable.jsx
+++ app/javascript/components/root/components/Software/SoftwareTable/SoftwareTable.jsx
@@ -4,19 +4,17 @@ import Badge from 'react-bootstrap/Badge';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Nav from 'react-bootstrap/Nav';
-import Tab from 'react-bootstrap/Tab';
 import Form from 'react-bootstrap/Form';
 import { fetchAllSoftware, postSoftware, putSoftware, deleteSoftware } from '../../../softwareService';
-import { StandardImage, StandardCard, StandardPage, getThemeColor, StandardCardHeader, StandardButton, StandardModal, StandardTextField, StandardCheckBox } from '../../Utility/Utility';
+import { StandardImage, StandardCard, StandardPage, getThemeColor, StandardButton, StandardModal, StandardTextField, StandardCheckBox, getIconSizing } from '../../Utility/Utility';
 import { AppContext } from '../../../AppContext';
-import RecentIcon from 'react-bootstrap-icons/dist/icons/layers';
-import AllIcon from 'react-bootstrap-icons/dist/icons/book';
+import Add from 'react-bootstrap-icons/dist/icons/plus-circle';
+import Edit from 'react-bootstrap-icons/dist/icons/pencil';
 
-function SoftwareTable({ userData }) {
-  const [software, setSoftware] = useState([]);
+function SoftwareTable({ userData, setActiveApplication }) {
   const appSettings = useContext(AppContext);
-  const { standardCardTitleFontSize } = appSettings;
+  const { fgColorDetail } = appSettings;
+  const [software, setSoftware] = useState([]);
   useEffect(() => {
     const loadSoftware = async () => {
       await fetchAllSoftware(setSoftware);
@@ -26,19 +24,33 @@ function SoftwareTable({ userData }) {
 
   return (
     <StandardPage title='Software Panel'>
-      <SoftwareSwitcher>
-        <Tab.Pane eventKey='Recent'>
-          <div style={{ fontSize: standardCardTitleFontSize, marginTop: '1vh' }}>Recent Software</div>
-          <StandardCardHeader />
-          <CustomTable userData={userData} software={software.filter(x => x.is_legacy === false)} setSoftware={setSoftware} />
-        </Tab.Pane>
-        <Tab.Pane eventKey='All'>
-          <div style={{ fontSize: standardCardTitleFontSize, marginTop: '1vh' }}>All Software</div>
-          <StandardCardHeader />
-          <CustomTable userData={userData} software={[...software]} setSoftware={setSoftware} />
-        </Tab.Pane>
-        {userData && <EditSoftware setSoftware={setSoftware} />}
-      </SoftwareSwitcher>
+      <div style={{ width: '95%', margin: 'auto' }}>
+        <Table size='sm' hover style={{ color: 'white', backgroundColor: fgColorDetail, border: `1px solid ${getThemeColor(0.1)}` }}>
+          <thead>
+            <tr>
+              <th style={{ border: `1px solid ${getThemeColor(0.1)}` }}>Name</th>
+              <th style={{ border: `1px solid ${getThemeColor(0.1)}` }}>Language</th>
+              <th style={{ border: `1px solid ${getThemeColor(0.1)}` }}>Platform(s)</th>
+              <th style={{ border: `1px solid ${getThemeColor(0.1)}` }}>Year</th>
+              {userData && <th style={{ border: `1px solid ${getThemeColor(0.1)}` }}></th>}
+            </tr>
+          </thead>
+          <tbody>
+            {software?.map((app) => {
+              return (
+                <SoftwareSample
+                  key={app.id}
+                  software={app}
+                  userData={userData}
+                  setSoftware={setSoftware}
+                  setActiveApplication={setActiveApplication}
+                />
+              );
+            })}
+          </tbody>
+        </Table>
+      </div>
+      {userData && <EditSoftware setSoftware={setSoftware} />}
     </StandardPage>
   );
 }
@@ -57,6 +69,7 @@ const EditSoftware = ({ software: existingSoftware, setSoftware: setSoftwares })
     android: false,
     repo_link: '',
     languages: '',
+    youtube_link: ''
   };
   const [software, setSoftware] = useState(existingSoftware ? existingSoftware : blankSoftware);
 
@@ -92,7 +105,7 @@ const EditSoftware = ({ software: existingSoftware, setSoftware: setSoftwares })
 
   return (
     <>
-      <StandardButton onClick={() => setModalOpen(true)} style={{ minWidth: '100%' }}>{existingSoftware ? 'Edit' : 'Add Application'}</StandardButton>
+      <StandardButton icon={existingSoftware ? <Edit style={{ fontSize: getIconSizing('small') }} /> : <Add style={{ fontSize: getIconSizing('small') }} />} onClick={() => setModalOpen(true)}>{existingSoftware ? 'Edit' : 'Add Application'}</StandardButton>
 
       <StandardModal buttons={existingSoftware ? ExistingButtons : NewButtons} modalOpen={modalOpen} handleModalClose={() => setModalOpen(false)}>
 
@@ -104,6 +117,7 @@ const EditSoftware = ({ software: existingSoftware, setSoftware: setSoftwares })
             <StandardTextField value={software.description} rows={4} label='Description' onChange={(e) => setSoftware({ ...software, description: e.target.value })} />
             <StandardTextField value={software.repo_link} label='Repository Link' onChange={(e) => setSoftware({ ...software, repo_link: e.target.value })} />
             <StandardTextField value={software.languages} label='Languages' onChange={(e) => setSoftware({ ...software, languages: e.target.value })} />
+            <StandardTextField value={software.youtube_link} label='Youtube Video Identifier' onChange={(e) => setSoftware({ ...software, youtube_link: e.target.value })} />
             <StandardCheckBox label='Is this legacy software?' value={software.is_legacy} onChange={() => setSoftware({ ...software, is_legacy: !software.is_legacy })} />
             <StandardCheckBox label='Windows' value={software.windows} onChange={() => setSoftware({ ...software, windows: !software.windows })} />
             <StandardCheckBox label='Linux' value={software.linux} onChange={() => setSoftware({ ...software, linux: !software.linux })} />
@@ -117,75 +131,7 @@ const EditSoftware = ({ software: existingSoftware, setSoftware: setSoftwares })
   )
 }
 
-const SoftwareSwitcher = ({ children }) => {
-  const BlankKeys = { Recent: false, All: false }
-  const [activeKey, setActiveKey] = useState({ ...BlankKeys, Recent: true });
-
-  const CustomLink = ({ keyBool, keyText, displayText, icon }) => {
-    return (
-      <Nav.Link as={'div'} className={keyBool || 'defaultMouseOver'} style={keyBool ? { backgroundColor: getThemeColor(1), color: 'black', cursor: 'pointer' } : { cursor: 'pointer' }} onClick={() => setActiveKey({ ...BlankKeys, [keyText]: true })} eventKey={keyText}>{icon}<div />{displayText}</Nav.Link>
-    )
-  }
-
-  return (
-    <Tab.Container defaultActiveKey={'Recent'}>
-      <Row>
-        <Col sm={3}>
-          <StandardCard>
-            <Nav style={{ minWidth: '100%' }} variant='pills' className='flex-column'>
-              <Nav.Item>
-                <CustomLink icon={<RecentIcon />} keyBool={activeKey.Recent} keyText='Recent' displayText='Recent Software' />
-              </Nav.Item>
-              <Nav.Item>
-                <CustomLink icon={<AllIcon />} keyBool={activeKey.All} keyText='All' displayText='All Software' />
-              </Nav.Item>
-            </Nav>
-          </StandardCard>
-        </Col>
-        <Col sm={9}>
-          <Tab.Content>
-            {children}
-          </Tab.Content>
-        </Col>
-      </Row>
-    </Tab.Container>
-  )
-}
-
-const CustomTable = ({ software, userData, style, setSoftware }) => {
-  const appSettings = useContext(AppContext);
-  const { fgColorDetail } = appSettings;
-
-  return (
-    <div style={{ ...style, width: '95%', margin: 'auto' }}>
-      <Table size='sm' hover style={{ ...style, color: 'white', backgroundColor: fgColorDetail, border: `1px solid ${getThemeColor(0.2)}` }}>
-        <thead>
-          <tr>
-            <th style={{ border: `1px solid ${getThemeColor(0.2)}` }}>Name</th>
-            <th style={{ border: `1px solid ${getThemeColor(0.2)}` }}>Language</th>
-            <th style={{ border: `1px solid ${getThemeColor(0.2)}` }}>Platform(s)</th>
-            <th style={{ border: `1px solid ${getThemeColor(0.2)}` }}>Year</th>
-            {userData && <th style={{ border: `1px solid ${getThemeColor(0.2)}` }}></th>}
-          </tr>
-        </thead>
-        <tbody>
-          {software?.map((app) => {
-            return (
-              <SoftwareSample
-                key={app.id}
-                software={app}
-                userData={userData}
-                setSoftware={setSoftware}
-              />
-            );
-          })}
-        </tbody>
-      </Table>
-    </div>
-  )
-}
-
-const SoftwareSample = ({ software, userData, setSoftware }) => {
+const SoftwareSample = ({ software, userData, setSoftware, setActiveApplication }) => {
   const compatibility = {
     windows: software.windows,
     linux: software.linux,
@@ -193,25 +139,29 @@ const SoftwareSample = ({ software, userData, setSoftware }) => {
     android: software.android
   };
 
+  const CustomTd = ({ children, style, noOnClick = false }) => {
+    return (
+      <td style={{ borderTop: `1px solid ${getThemeColor(0.1)}`, verticalAlign: 'middle', ...style }} onClick={() => noOnClick ? () => { } : setActiveApplication(software.name)}>
+        {children}
+      </td>
+    )
+  }
+
   return (
     <tr className='defaultMouseOver' style={{ cursor: 'pointer' }}>
-      <td style={{ borderTop: `1px solid ${getThemeColor(0.2)}` }} onClick={() => window.open(`/software/${software.name}`, '_self')}>
-        <StandardImage src={software.icon_link} style={{ maxWidth: '6%' }} />
+      <CustomTd style={{ width: '35%' }}>
+        <StandardImage src={software.icon_link} style={{ width: '10%' }} />
         {software.name}
-      </td>
-      <td style={{ borderTop: `1px solid ${getThemeColor(0.2)}` }} onClick={() => window.open(`/software/${software.name}`, '_self')}>
-        {software.languages}
-      </td>
-      <td style={{ borderTop: `1px solid ${getThemeColor(0.2)}` }} onClick={() => window.open(`/software/${software.name}`, '_self')}>
+      </CustomTd>
+      <CustomTd>{software.languages}</CustomTd>
+      <CustomTd>
         {compatibility.windows && <Badge variant='dark'>Windows</Badge>}{' '}
         {compatibility.linux && <Badge variant='dark'>Linux</Badge>}{' '}
         {compatibility.mac && <Badge variant='dark'>Mac</Badge>}{' '}
         {compatibility.android && <Badge variant='dark'>Android</Badge>}{' '}
-      </td>
-      <td style={{ borderTop: `1px solid ${getThemeColor(0.2)}` }}>'20</td>
-      {userData && <td style={{ borderTop: `1px solid ${getThemeColor(0.2)}` }}><EditSoftware setSoftware={setSoftware} software={software} /></td>}
-
-
+      </CustomTd>
+      <CustomTd>2019</CustomTd>
+      {userData && <CustomTd noOnClick><EditSoftware setSoftware={setSoftware} software={software} /></CustomTd>}
     </tr>
   );
 };
