ed2d09245b804763f25f75f8b523d54f22a3a308
aaron <aprathdev@gmail.com>
2020-07-19 20:54:09 -0400

    Move background animation assets elsewhere, remove unused assets, animation tweak

--- app/javascript/components/root/Root.js
+++ app/javascript/components/root/Root.js
@@ -2,8 +2,6 @@ import React, { useContext } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { AppContext } from './AppContext';
-import stars from './images/stars.png';
-import blackhole from './images/blackhole.png';
 import './components/style.css';
 import MainWrapper from './components/MainWrapper';
 import './components/Mainpage/SiteLogo/SiteLogo.css';
@@ -26,10 +24,9 @@ function Root() {
             userSelect: 'none'
           }}
         >
-          <div className='starCW' style={{ position: 'absolute', width: '100%', height: '100%', backgroundImage: `url(${stars})` }}></div>
-          <div className='starCWLag' style={{ position: 'absolute', width: '100%', height: '100%', backgroundImage: `url(${stars})` }}></div>
-          <div className='starCCW' style={{ position: 'absolute', width: '100%', height: '100%', backgroundImage: `url(${stars})` }}></div>
-          <div className='blackhole' style={{ position: 'absolute', width: '100%', height: '100%', backgroundImage: `url(${blackhole})` }} />
+          <div className='starCW' style={{ position: 'absolute', width: '100%', height: '100%', backgroundImage: `url(https://i92.servimg.com/u/f92/11/29/62/29/stars10.png)`, opacity: 1 }}></div>
+          <div className='starCCW' style={{ position: 'absolute', width: '100%', height: '100%', backgroundImage: `url(https://i92.servimg.com/u/f92/11/29/62/29/stars10.png)`, opacity: 0.75 }}></div>
+          <div className='blackhole' style={{ position: 'absolute', width: '100%', height: '100%', backgroundImage: `url(https://i92.servimg.com/u/f92/11/29/62/29/blackh10.png)` }} />
           <Switch>
             <Route exact path='/software/:name' component={(props) => <MainWrapper activeApplication={props.match.params.name} activeKey='Software' />} />
           </Switch>
