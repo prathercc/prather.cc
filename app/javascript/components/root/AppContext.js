aaad16f50b4cbcd03b29d38a2e7d8ac8e04804fc
Aaron <aprathdev@gmail.com>
2020-07-27 22:52:45 -0400

    Clear out garbage from AppContext + standardize some remaining names

--- app/javascript/components/root/AppContext.js
+++ app/javascript/components/root/AppContext.js
@@ -4,21 +4,10 @@ const appSettings = {
   bgColor: '#1a1a1a',
   fgColorDetail: 'rgb(79, 201, 201, 0.1)',
   fgColor: 'rgb(79, 201, 201, 0.1)',
-  textColor: '#FFFFFF',
   fontStyle: 'Titillium Web',
-  iconSizing: '3vw',
-  iconSizingSmall: '3vw',
-
-  appbarFontSize: 'calc(5px + 2vmin)',
-
-  tableHeaderFontSize: 'calc(3.5px + 2vmin)',
-  tableNotesFontSize: 'calc(5px + 1vmin)',
-
-  softwareFontSize: 'calc(1px + 2vmin)',
-  softwareMaintenanceFontSize: 'calc(0.5px + 2vmin)',
-
-  standardPageTitleFontSize: 'calc(5px + 2vmin)',
-  standardCardTitleFontSize: 'calc(3px + 2vmin)'
+  standardTitleFontSize: 'calc(3px + 2vmin)',
+  standardBodyFontSize: 'calc(1px + 2vmin)',
+  standardSmallFontSize: 'calc(1px + 1.75vmin)'
 };
 
 export const AppContext = createContext(appSettings);
