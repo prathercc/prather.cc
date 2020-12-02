cf7b51e92a998fdbc8e626f39dd3079686f5b4bb
Aaron <aprathdev@gmail.com>
2020-12-01 20:55:25 -0500

    Replace react-bootstrap Table with the Antd Table

--- app/javascript/components/root/components/Software/SoftwareTable/SoftwareTable.jsx
+++ app/javascript/components/root/components/Software/SoftwareTable/SoftwareTable.jsx
@@ -1,72 +1,68 @@
 import React, { useEffect, useState } from 'react';
-import Table from 'react-bootstrap/Table';
-import Form from 'react-bootstrap/Form';
 import { fetchAllSoftware, postSoftware, putSoftware, deleteSoftware } from '../../../softwareService';
-import { StandardImage, StandardDatePicker, StandardPage, getThemeColor, StandardButton, StandardModal, StandardTextField, StandardCheckBox, getIconSizing, StandardIconButton, StandardTooltip } from '../../Utility/Utility';
-import { MDBIcon } from "mdbreact";
-import { Row, Col, Spin } from 'antd';
+import { StandardPage, StandardButton, StandardModal, StandardIconButton, toggleNotification } from '../../Utility/Utility';
+import { Row, Col, Spin, Form, Input, DatePicker, Checkbox, Table } from 'antd';
+import { PlusOutlined, EditOutlined } from '@ant-design/icons';
+import moment from 'moment';
 
-function SoftwareTable({ userData, setActiveApplication, displayAlert }) {
+function SoftwareTable({ userData, setActiveApplication }) {
   const [software, setSoftware] = useState(null);
-  const [sortDir, setSortDir] = useState('desc');
+  const [softwareIsLoading, setSoftwareIsLoading] = useState(true);
 
   useEffect(() => {
     const loadSoftware = async () => {
-      const { data } = await fetchAllSoftware();
-      setSoftware(data);
+      try {
+        const { data } = await fetchAllSoftware();
+        setSoftware(data);
+      }
+      catch (e) {
+        console.error('Failed to load software: ', e);
+      }
+      finally {
+        setSoftwareIsLoading(false);
+      }
     };
     loadSoftware();
   }, []);
 
-  const CustomTh = ({ children, name, sortable = true }) => {
-    const handleSort = async () => {
-      if (sortDir === 'desc')
-        setSortDir('asc');
-      else
-        setSortDir('desc');
-      const { data } = await fetchAllSoftware(name, sortDir);
-      setSoftware(data);
+  useEffect(() => {
+    const assignCompatibility = async (data) => {
+      setSoftwareIsLoading(true);
+      const updatedData = data.map(x => (
+        {
+          ...x, dev_date_display: `${getSeason(new Date(x.dev_date).getMonth())} ${new Date(x.dev_date).getFullYear()}`,
+          adminEdit: <EditSoftware setSoftware={setSoftware} software={x} />
+        }
+      ));
+      setSoftware(updatedData);
+      setSoftwareIsLoading(false);
     }
-    return (
-      <th style={{ border: 'none', backgroundColor: getThemeColor(0), fontWeight: 'normal', color: getThemeColor(1) }}>
-        <div onClick={sortable ? handleSort : () => { }} className={sortable ? 'tableHeaderMouseOver' : 'tableHeaderNotSortable'}>{children}</div>
-      </th>
-    );
-  };
+
+    if (software?.length > 0 && !software[0]?.adminEdit)
+      assignCompatibility(software)
+
+  }, software);
+
+  const columns = [{
+    title: 'Name', dataIndex: 'name', key: 'name', align: 'center', sorter: (a, b) => a.name.length - b.name.length,
+    sortDirections: ['descend', 'ascend']
+  },
+  {
+    title: 'Development Date', dataIndex: 'dev_date_display', key: 'dev_date', align: 'center', sorter: (a, b) => new Date(a.dev_date) - new Date(b.dev_date),
+    sortDirections: ['descend', 'ascend']
+  }];
+  const adminColumns = [...columns, { title: 'Actions', dataIndex: 'adminEdit', key: 'adminEdit', align: 'center' }];
 
   return (
-    <StandardPage title='Software Panel'>
-      <Table size='sm' hover style={{ color: 'white', backgroundColor: 'transparent', marginTop: '1vh', display: software ? '' : 'none' }}>
-        <thead>
-          <tr>
-            <CustomTh name='name'>Name</CustomTh>
-            <CustomTh name='dev_date'>Development Date</CustomTh>
-            <CustomTh sortable={false}>Compatibility</CustomTh>
-            {userData?.group === 'Administrator' && <CustomTh />}
-          </tr>
-        </thead>
-        <tbody>
-          {software?.map((app) => {
-            return (
-              <SoftwareSample
-                key={app.id}
-                software={app}
-                userData={userData}
-                setSoftware={setSoftware}
-                setActiveApplication={setActiveApplication}
-                displayAlert={displayAlert}
-              />
-            );
-          })}
-        </tbody>
-      </Table>
-      {!software && <Spin style={{ marginTop: '1vh' }} />}
-      {userData?.group === 'Administrator' && <EditSoftware displayAlert={displayAlert} setSoftware={setSoftware} />}
+    <StandardPage title='Software Table'>
+      <Table style={{ marginTop: '5px', padding: '5px' }} pagination={false} onRow={(record) => { return { onClick: event => { if (event.target.tagName === 'TD') setActiveApplication(record.name); } } }} dataSource={software} columns={userData?.group === 'Administrator' && adminColumns || columns} />
+      {softwareIsLoading && <Spin style={{ marginTop: '1vh' }} />}
+      {userData?.group === 'Administrator' && <EditSoftware setSoftware={setSoftware} />}
     </StandardPage>
   );
 };
 
-const EditSoftware = ({ software: existingSoftware, setSoftware: setSoftwares, displayAlert }) => {
+const EditSoftware = ({ software: existingSoftware, setSoftware: setSoftwares }) => {
   const [modalOpen, setModalOpen] = useState(false);
   const blankSoftware = {
     is_legacy: false,
@@ -85,7 +81,8 @@ const EditSoftware = ({ software: existingSoftware, setSoftware: setSoftwares, d
   };
   const [software, setSoftware] = useState(existingSoftware ? existingSoftware : blankSoftware);
   const isSubmitDisabled = software.name.length === 0 || software.icon_link.length === 0 || software.description.length === 0 ||
-    software.image_link.length === 0 || software.repo_link.length === 0 || software.languages.length === 0
+    software.repo_link.length === 0 || software.languages.length === 0
+  const [form] = Form.useForm();
 
   const handleCreateSoftware = async () => {
     const { data } = await postSoftware(software);
@@ -93,11 +90,12 @@ const EditSoftware = ({ software: existingSoftware, setSoftware: setSoftwares, d
       const { data } = await fetchAllSoftware();
       setSoftwares(data);
       setSoftware(blankSoftware);
+      form.setFieldsValue(blankSoftware);
       setModalOpen(false);
-      displayAlert('Successfully saved software', true);
+      toggleNotification('success', 'Success', 'Successfully created application!');
     }
     else {
-      displayAlert('Network error while saving software', false);
+      toggleNotification('error', 'Failure', 'Failed to create application!');
     }
   };
   const handleEditSoftware = async () => {
@@ -106,10 +104,10 @@ const EditSoftware = ({ software: existingSoftware, setSoftware: setSoftwares, d
       const { data } = await fetchAllSoftware();
       setSoftwares(data);
       setModalOpen(false);
-      displayAlert('Successfully saved software', true);
+      toggleNotification('success', 'Success', 'Successfully saved application!');
     }
     else {
-      displayAlert('Network error while saving software', false);
+      toggleNotification('error', 'Failure', 'Failed to save application!');
     }
   };
   const handleDeleteSoftware = async () => {
@@ -118,10 +116,10 @@ const EditSoftware = ({ software: existingSoftware, setSoftware: setSoftwares, d
       const { data } = await fetchAllSoftware();
       setSoftwares(data);
       setModalOpen(false);
-      displayAlert('Successfully deleted software', true);
+      toggleNotification('success', 'Success', 'Successfully deleted application!');
     }
     else {
-      displayAlert('Network error while deleting software', false);
+      toggleNotification('error', 'Failure', 'Failed to delete application!');
     }
   };
 
@@ -155,84 +153,72 @@ const EditSoftware = ({ software: existingSoftware, setSoftware: setSoftwares, d
   };
 
   return (
-    <>
+    <div>
       {existingSoftware &&
-        <StandardIconButton onClick={() => setModalOpen(true)} toolTip='Edit Software' icon={<MDBIcon icon="pencil-alt" />} />
+        <StandardIconButton onClick={() => setModalOpen(true)} toolTip='Edit Software' icon={<EditOutlined />} />
       }
       {!existingSoftware &&
-        <StandardIconButton onClick={() => setModalOpen(true)} toolTip='Add Software' icon={<MDBIcon icon="plus" />} />
+        <StandardIconButton onClick={() => setModalOpen(true)} toolTip='Add Software' icon={<PlusOutlined />} />
       }
       <StandardModal title={`Software Alteration - ${!existingSoftware ? 'Create' : 'Modify'}`} buttons={existingSoftware ? <ExistingButtons /> : <NewButtons />} modalOpen={modalOpen} handleModalClose={() => setModalOpen(false)}>
-        <Form.Group style={{ width: '95%', margin: 'auto' }}>
-          <StandardTextField hasError={software.name.length === 0} value={software.name} isActive={!existingSoftware} label='Name' onChange={(e) => setSoftware({ ...software, name: e.target.value })} />
-          <StandardTextField hasError={software.icon_link.length === 0} value={software.icon_link} label='Icon Image Link' onChange={(e) => setSoftware({ ...software, icon_link: e.target.value })} />
-          <StandardTextField hasError={software.image_link.length === 0} value={software.image_link} label='Image Link' onChange={(e) => setSoftware({ ...software, image_link: e.target.value })} />
-          <StandardTextField hasError={software.description.length === 0} value={software.description} rows={4} label='Description' onChange={(e) => setSoftware({ ...software, description: e.target.value })} />
-          <StandardTextField hasError={software.repo_link.length === 0} value={software.repo_link} label='Repository Link' onChange={(e) => setSoftware({ ...software, repo_link: e.target.value })} />
-          <StandardTextField hasError={software.languages.length === 0} value={software.languages} label='Languages' onChange={(e) => setSoftware({ ...software, languages: e.target.value })} />
-          <StandardTextField value={software.youtube_link} label='Youtube Video Identifier' onChange={(e) => setSoftware({ ...software, youtube_link: e.target.value })} />
-          <StandardDatePicker label='Development Date' date={new Date(software.dev_date)} setDate={(d) => setSoftware({ ...software, dev_date: d })} />
+        <Form initialValues={{ ...software }} onValuesChange={(e) => setSoftware({ ...software, ...e })} form={form} layout='vertical'>
+          <Form.Item name='name' label='Name'>
+            <Input placeholder='Type a name' />
+          </Form.Item>
+          <Form.Item name='icon_link' label='Icon Image Link'>
+            <Input placeholder='Type a URL' />
+          </Form.Item>
+          <Form.Item name='description' label='Description'>
+            <Input.TextArea rows={4} placeholder='Type a description' />
+          </Form.Item>
+          <Form.Item name='repo_link' label='Repository Link'>
+            <Input placeholder='Type a URL' />
+          </Form.Item>
+          <Form.Item name='languages' label='Language(s)'>
+            <Input placeholder='Type a language' />
+          </Form.Item>
+          <Form.Item name='youtube_link' label='Youtube Link'>
+            <Input placeholder='Type a URL' />
+          </Form.Item>
+          <Form.Item>
+            <DatePicker defaultValue={moment(software.dev_date, 'YYYY-MM-DD')} onChange={(dt, dts) => setSoftware({ ...software, dev_date: dts })} />
+          </Form.Item>
           <Row>
-            <Col span={12}>
-              <StandardCheckBox label='Windows' value={software.windows} onChange={() => setSoftware({ ...software, windows: !software.windows })} />
+            <Col span={6}>
+              <Form.Item valuePropName='checked' name='windows'>
+                <Checkbox>Windows?</Checkbox>
+              </Form.Item>
             </Col>
-            <Col span={12}>
-              <StandardCheckBox label='Linux' value={software.linux} onChange={() => setSoftware({ ...software, linux: !software.linux })} />
+            <Col span={6}>
+              <Form.Item valuePropName='checked' name='linux'>
+                <Checkbox>Linux?</Checkbox>
+              </Form.Item>
+            </Col>
+            <Col span={6}>
+              <Form.Item valuePropName='checked' name='mac'>
+                <Checkbox>Mac?</Checkbox>
+              </Form.Item>
+            </Col>
+            <Col span={6}>
+              <Form.Item valuePropName='checked' name='android'>
+                <Checkbox>Android?</Checkbox>
+              </Form.Item>
             </Col>
           </Row>
-          <Row>
-            <Col span={12}>
-              <StandardCheckBox label='Mac' value={software.mac} onChange={() => setSoftware({ ...software, mac: !software.mac })} />
-            </Col>
-            <Col span={12}>
-              <StandardCheckBox label='Android' value={software.android} onChange={() => setSoftware({ ...software, android: !software.android })} />
-            </Col>
-          </Row>
-          <Row>
-            <Col span={24}>
-              <StandardCheckBox label='Legacy Application' value={software.is_legacy} onChange={() => setSoftware({ ...software, is_legacy: !software.is_legacy })} />
-            </Col>
-          </Row>
-        </Form.Group>
+          <Form.Item valuePropName='checked' name='is_legacy'>
+            <Checkbox>Is Legacy?</Checkbox>
+          </Form.Item>
+        </Form>
       </StandardModal>
-    </>
+    </div>
   );
 };
 
-const SoftwareSample = ({ software, userData, setSoftware, setActiveApplication, displayAlert }) => {
-  const CustomTd = ({ children, style, noOnClick = false }) => {
-    return (
-      <td style={{ borderTop: `1px solid ${getThemeColor(0.1)}`, verticalAlign: 'middle', ...style }} onClick={() => noOnClick ? () => { } : setActiveApplication(software.name)}>
-        {children}
-      </td>
-    );
-  };
-  const months = ["January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"
-  ];
-  const developmentDate = new Date(software.dev_date);
-  const dateDisplayString = months[developmentDate.getMonth()] + ' ' + developmentDate.getFullYear();
-  const isWebApplication = !software.windows && !software.linux && !software.mac && !software.android;
-  const [imageLoading, setImageLoading] = useState(true);
-  const compatibilityStyling = { marginLeft: '3px', marginRight: '3px' };
-
-  return (
-    <tr style={{ opacity: imageLoading ? 0 : 1 }} className='tableMouseOver'>
-      <CustomTd style={{ width: '50%' }}>
-        <StandardImage noLoader onLoaded={() => setImageLoading(false)} noErrorMessage src={software.icon_link} style={{ width: getIconSizing() }} />
-        {software.name}
-      </CustomTd>
-      <CustomTd>{dateDisplayString}</CustomTd>
-      <CustomTd>
-        {software.windows && <StandardTooltip text='Windows'><MDBIcon style={compatibilityStyling} className='defaultMouseOver' fab icon="windows" /></StandardTooltip>}
-        {software.linux && <StandardTooltip text='Linux'><MDBIcon style={compatibilityStyling} className='defaultMouseOver' fab icon="linux" /></StandardTooltip>}
-        {software.mac && <StandardTooltip text='Apple'><MDBIcon style={compatibilityStyling} className='defaultMouseOver' fab icon="apple" /></StandardTooltip>}
-        {software.android && <StandardTooltip text='Android'><MDBIcon style={compatibilityStyling} className='defaultMouseOver' fab icon="android" /></StandardTooltip>}
-        {isWebApplication && <StandardTooltip text='Web Application'><MDBIcon style={compatibilityStyling} className='defaultMouseOver' fab icon="chrome" /></StandardTooltip>}
-      </CustomTd>
-      {userData?.group === 'Administrator' && <CustomTd noOnClick><EditSoftware displayAlert={displayAlert} setSoftware={setSoftware} software={software} /></CustomTd>}
-    </tr>
-  );
-};
+const getSeason = (month) => {
+  const winterArr = [11, 12, 1, 2];
+  const springArr = [3, 4];
+  const summerArr = [5, 6, 7];
+  return winterArr.includes(month) ? 'Winter' : springArr.includes(month) ? 'Spring' : summerArr.includes(month) ? 'Summer' : 'Fall';
+}
 
 export default SoftwareTable;
