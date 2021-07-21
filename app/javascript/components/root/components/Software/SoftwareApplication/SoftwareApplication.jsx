7478291cc5e37efa44530f78de25848dff38b296
Aaron <aprathdev@gmail.com>
2021-07-20 21:14:26 -0400

    Adjust SoftwareApplication display formatting

--- app/javascript/components/root/components/Software/SoftwareApplication/SoftwareApplication.jsx
+++ app/javascript/components/root/components/Software/SoftwareApplication/SoftwareApplication.jsx
@@ -184,12 +184,12 @@ function SoftwareApplication({ userData, name }) {
               </Descriptions.Item>
             )}
             {features &&
-              features.map((feature, i) => {
+              features.map((feature) => {
                 return (
                   <>
                     <Descriptions.Item
-                      span={2}
-                      label={`Feature ${i + 1} - ${feature.title}`}
+                      span={1}
+                      label={`${feature.title}`}
                     >
                       <StandardImage
                         className="defaultImageNudge"
@@ -226,7 +226,7 @@ function SoftwareApplication({ userData, name }) {
                         </div>
                       )}
                     </Descriptions.Item>
-                    <Descriptions.Item span={1} label={``}>
+                    <Descriptions.Item span={2} label={``}>
                       <div
                         dangerouslySetInnerHTML={{
                           __html: feature.content_description,
